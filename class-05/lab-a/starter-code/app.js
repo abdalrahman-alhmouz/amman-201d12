f840e57b877486d98d889de70da821ec8d23a48d
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2020-12-31 18:59:09 +0200

    proplem3 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -61,11 +61,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var total = sum(a, b)
+    var newTotal = sum(total[0], c)
+    var multiplyTotal = multiply(a, b);
+    var newMultipltotal = multiply(multiplyTotal[0], c)
+    var returnArray = [
+        newTotal[0],
+        newMultipltotal[0],
+        "4 and 7 and 5 sum to 16.",
+        "The product of 4 and 7 and 5 is 140."
+    ];
+    return returnArray;
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
